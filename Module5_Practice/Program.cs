54fc0c3545ee5bc0d0c07e259ca2ffbf65371910
Tulpar <ibrahim.mak777@gmail.com>
2026-02-22 13:26:02 +0500

    hg

--- Module5_Practice/Program.cs
+++ Module5_Practice/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.Text;
 using System.Text.Json;
-using System.Collections.Generic;
-using System.Threading;
+
 
 // ===== SINGLETON =========
 
